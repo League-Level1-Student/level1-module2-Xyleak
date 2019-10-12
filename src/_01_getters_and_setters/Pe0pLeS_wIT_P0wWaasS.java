b258d466be44ea8610c6be5e2cbd70dcf77da8d4
league <league@imac-9.attlocal.net>
2019-10-12 11:26:13 -0700

    getters and setters

---
+++ src/_01_getters_and_setters/Pe0pLeS_wIT_P0wWaasS.java
@@ -0,0 +1,23 @@
+package _01_getters_and_setters;
+
+public class Pe0pLeS_wIT_P0wWaasS {
+
+	private String Sophia_Soap;
+	private String Koney_Starch;
+	private String BillyBobJoe;
+
+
+
+	void getSophiaSoap(String Sophia_Soap){
+		return;
+	}
+
+	void getKoney_Starch(String Koney_Starch) {
+		return;
+	}
+
+	void getBillyBobJoe(String BillyBobJoe) {
+		return;	
+	}
+
+}
---
+++ src/_01_getters_and_setters/tAcOS_R_bAd_4_U_.java
@@ -0,0 +1,27 @@
+package _01_getters_and_setters;
+
+public class tAcOS_R_bAd_4_U_ {
+
+private String Meat;
+private String Sauce;	
+	
+	
+void getMeat(){
+	return;
+}
+
+
+void getSauce(){
+	return;
+}
+	
+void setMeat(String Meat){
+	Meat.equals("1996478564854486567765");
+
+}
+
+void setSauce(String Sauce){
+	Sauce.equals("42567666879");
+}
+
+}
