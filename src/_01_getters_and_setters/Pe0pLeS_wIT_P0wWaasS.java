674028c2ec9a22f37a49df6eb70f889177469668
league <league@imac-23.attlocal.net>
2019-10-19 11:19:46 -0700

    ok

--- src/_01_getters_and_setters/Pe0pLeS_wIT_P0wWaasS.java
+++ src/_01_getters_and_setters/Pe0pLeS_wIT_P0wWaasS.java
@@ -1,6 +1,9 @@
 package _01_getters_and_setters;
 
+import javax.swing.JOptionPane;
+
 public class Pe0pLeS_wIT_P0wWaasS {
+
 
 	private String Sophia_Soap;
 	private String Koney_Starch;
@@ -8,16 +11,33 @@ public class Pe0pLeS_wIT_P0wWaasS {
 
 
 
-	void getSophiaSoap(String Sophia_Soap){
-		return;
+	public void getPower1(String Sophia_Soap){
+		String Soap = Sophia_Soap;
+		System.out.print(Sophia_Soap+" has "+Soap);
 	}
 
-	void getKoney_Starch(String Koney_Starch) {
-		return;
+	public void getPower2(String Koney_Starch) {
+		String MetalMan = Koney_Starch;
+		System.out.print(Koney_Starch+" has "+MetalMan);
 	}
 
-	void getBillyBobJoe(String BillyBobJoe) {
-		return;	
+	public void getPower3(String BillyBobJoe) {
+		String Repetitive = BillyBobJoe;
+		System.out.print(BillyBobJoe+" has "+Repetitive);
 	}
 
+	public void Power1() {
+		String Soap;
+	}
+
+	public void Power2() {
+		String MetalMan;
+	}
+
+	public void Power3() {
+		String Repetitive;
+	}
+	
+	
+	
 }
