602a3280dc937dba8208a6e6135c3d763c5a4c29
league <league@imac-16.attlocal.net>
2019-11-16 11:05:19 -0800

    Tea

--- src/_07_tea_party/Fermentation_Of_Dehydrated_Greenery.java
+++ src/_07_tea_party/Fermentation_Of_Dehydrated_Greenery.java
@@ -1,23 +1,32 @@
 package _07_tea_party;
-	
-	public class Fermentation_Of_Dehydrated_Greenery {
 
-		public String welcome(String name, boolean isWoman, boolean isKnighted) {
-        
-        
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Fermentation_Of_Dehydrated_Greenery {
+
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		return name;
 
 
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	}
+	class TeaPartyTest {
+		/**
+		 * Jane Austen is a woman, so say “Hello Ms. Austen”. 
+		 * George Orwell is a man, so say “Hello Mr. Orwell”. 
+		 * Isaac Newton was knighted, so say "Hello Sir Isaac Newton".
+		 **/
+
+		@Test
+		public void test() {
+			Fermentation_Of_Dehydrated_Greenery teaParty = new Fermentation_Of_Dehydrated_Greenery();
+			String greeting = teaParty.welcome(null, false, false);
+			assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false));
+			assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false));
+			assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true));
+
+
+		}
 	}
+}
